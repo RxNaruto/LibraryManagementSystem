acc134c37b4e4b33a35e19d3e6304f399fbaf638
Rithk <rithkchaudhary86@gmail.com>
2024-07-01 22:05:02 +0530

    plugins

--- backend/dist/config.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.JWT_SECRET2 = exports.JWT_SECRET1 = void 0;
-exports.JWT_SECRET1 = "mysecretkey";
-exports.JWT_SECRET2 = "adminsecretkey";
